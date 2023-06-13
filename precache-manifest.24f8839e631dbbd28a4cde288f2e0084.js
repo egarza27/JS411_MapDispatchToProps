2dc792b5fd559588afaa0bbf7d3a8f09635b8e9e
egarza27 <elizabethigarza10@gmail.com>
2023-06-12 19:47:20 -0500

    Updates

---
+++ precache-manifest.24f8839e631dbbd28a4cde288f2e0084.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3451e924adf0d26878eac524b84c5d91",
+    "url": "/JS411_MapDispatchToProps/index.html"
+  },
+  {
+    "revision": "ff6fdb41c91c2d19f167",
+    "url": "/JS411_MapDispatchToProps/static/css/main.71035251.chunk.css"
+  },
+  {
+    "revision": "38bc6ebe2274aba0d644",
+    "url": "/JS411_MapDispatchToProps/static/js/2.ba802b31.chunk.js"
+  },
+  {
+    "revision": "ff6fdb41c91c2d19f167",
+    "url": "/JS411_MapDispatchToProps/static/js/main.6a7e1de5.chunk.js"
+  },
+  {
+    "revision": "57da71dd5a8580e0d0e1",
+    "url": "/JS411_MapDispatchToProps/static/js/runtime~main.7cc3096f.js"
+  }
+]);
